49124f233ceda628b2c2212832f311efd85bdbc3
ND Morsalin <morsalin056@gmail.com>
2023-04-11 12:00:58 -0700

    filtering is complete

--- src/Pages/ApplidedJob/AppliedJob.jsx
+++ src/Pages/ApplidedJob/AppliedJob.jsx
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PagesHeader from "../../Components/PagesHeader/PagesHeader";
 import useGetAppliedJobs from "../../hooks/useGetAppliedJobs";
 import AppliedJobsCard from "../../Components/AppliedJobsCard/AppliedJobsCard";
 
 const AppliedJob = () => {
-  const appliedJob = useGetAppliedJobs();
-  // console.log(appliedJob);
+  const storedJob = useGetAppliedJobs();
+
+  const [appliedJobs, setAppliedJobs] = useState([]);
+
+  useEffect(() => {
+    setAppliedJobs(storedJob);
+  }, [storedJob]);
+
+  const handleFiltering = (option) => {
+    if (option) {
+      const filtered = storedJob.filter(
+        (job) => job.remote_or_onsite === option
+      );
+      setAppliedJobs(filtered);
+    } else {
+      setAppliedJobs(storedJob);
+    }
+  };
   return (
     <>
       <PagesHeader pageTitle=" Applied job" />
+      <div className="ml-auto w-fit py-2 px-4 rounded border border-blue-600 my-8">
+        <select
+          onChange={(e) => handleFiltering(e.target.value)}
+          defaultValue=""
+          name="filter"
+          id="filter"
+          className=""
+        >
+          <option disabled>Filter by</option>
+          <option value="">Select All</option>
+          <option value="Onsite">One Site</option>
+          <option value="Remote">Remote</option>
+        </select>
+      </div>
       <div className="my-8 space-y-4">
-        {appliedJob.map((job) => (
+        {appliedJobs.map((job) => (
           <AppliedJobsCard key={job.id} job={job} />
         ))}
       </div>
